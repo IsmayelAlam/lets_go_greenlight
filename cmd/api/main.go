9ce74dec6c7803726f4ab42a0490ac93d46a2bc1
Ismayel Alam <ismayelalam0@gmail.com>
2025-08-15 21:04:12 +0200

    create movie model for database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	_ "github.com/lib/pq"
+	"lets_go.greenlight/internal/data"
 )
 
 const version = "1.0.0"
@@ -28,6 +29,7 @@ type config struct {
 type application struct {
 	config config
 	logger *slog.Logger
+	models data.Models
 }
 
 func main() {
@@ -55,6 +57,7 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	srv := &http.Server{
