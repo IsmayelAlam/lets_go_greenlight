7e5d3b53c54731d0e2bc4c9c27beba9f80753e29
Ismayel Alam <ismayelalam0@gmail.com>
2025-08-20 22:10:40 +0200

    add endpoint for activating user

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -50,6 +50,7 @@ func (m TokenModel) New(userID int64, ttl time.Duration, scope string) (*Token, 
 	err := m.Insert(token)
 	return token, err
 }
+
 func (m TokenModel) Insert(token *Token) error {
 	query := `INSERT INTO tokens (hash, user_id, expiry, scope) VALUES ($1, $2, $3, $4)`
 	args := []any{token.Hash, token.UserID, token.Expiry, token.Scope}
